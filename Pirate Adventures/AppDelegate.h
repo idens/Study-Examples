744cdd1c16f5a8c04250d0710ecf1257a35a1035
Admin <admin@Mac-Admin.local>
2015-01-22 19:00:44 +0300

    Initial Commit

---
+++ Pirate Adventures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventures
+//
+//  Created by Admin on 22.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventures
+//
+//  Created by Admin on 22.01.15.
+//  Copyright (c) 2015 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
